7b76a4b318df033fb5af98a128474423763a7d6c
kimhj <>
2019-06-28 20:33:35 +0900

    라우터 링크 스타일 추가

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/posts">Posts</router-link> |
       <router-link to="/posts/new" exact>New Post</router-link> |
-      <router-link to="/posts/detail" exact>Detail Post</router-link> |
+      <router-link to="/posts/:id" exact>Detail Post</router-link> |
     </div>
     <router-view/>
   </div>
@@ -19,5 +19,15 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/** router link style ==> vuejs에서 정의된 스타일, 일부 경로가 일치되면 자동으로 추가 되는 클래스(전방일치) **/
+.router-link-active {
+color: white;
+background-color: red;
+}
+/** router link style ==> vuejs에서 정의된 스타일, 모든 경로가 일치되면 자동으로 추가 되는 클래스 **/
+.router-link-exact-active {
+color: white;
+background-color: blue;
 }
 </style>
