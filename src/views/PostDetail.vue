a43bad6f781ec8cc4da85503cefbb84549e72324
kimhj <>
2019-06-28 20:53:20 +0900

    watch ==> 라우팅 링크를 통해 경로가 변경되는 것을 감시한다...!

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -21,12 +21,15 @@ export default {
   created() {
     this.fetchData();
   },
-  watch: {'$route': 'fetchData'},
+  // ★ router link가 변경되었다는 것을 감지하는 기능. refresh
+  // 변경되면 fetchData 호출
+  watch: {$route: 'fetchData'},
   methods: {
     fetchData() {
       this.post = null;
       this.loading = true;
       // id 하나를 가져온다.
+      // watch에서 감시하고 있다가 변경되면 fetchData() ==> this.$route 객체 호출
       Post.get(this.$route.params.id).then(data => {
         this.post = data;
         this.loading = false;
